db8b8fba4669f75f3d91207d89a5ad1b6c398e06
Prijal Bista <prijalbista3@gmail.com>
2024-04-28 19:00:56 +0545

    feat: added + and - buttons along with interval input which increases or decreases bpm based on interval selected. eg- clicking - when interval is 5 will decrease the bpm by 5

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 let bpmInput = document.getElementById('bpm');
+let intervalInput = document.getElementById('interval');
 let playBtnDiv = document.querySelector('#play-btn>div');
 
 var metronome = new Metronome();
-const metronomeStart = () => {
-  metronome.tempo = parseInt(bpmInput.value);
-  metronome.start();
-};
-
-const metronomeStop = () => {
-  metronome.stop();
-};
 
 const metronomeToggleStartStop = () => {
   if (metronome.isRunning) {
@@ -20,4 +13,17 @@ const metronomeToggleStartStop = () => {
     metronome.start();
     playBtnDiv.className = 'pause';
   }
+};
+
+const updateBpm = (type) => {
+  let updatedTempo =
+    type === 'inc'
+      ? metronome.tempo + parseInt(intervalInput.value)
+      : metronome.tempo - parseInt(intervalInput.value);
+  updatedTempo = parseInt(updatedTempo);
+
+  if (!updatedTempo) return;
+
+  metronome.tempo = updatedTempo;
+  bpmInput.value = updatedTempo;
 };
